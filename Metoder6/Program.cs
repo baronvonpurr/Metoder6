4e59f54c9cb4bd9f22220d9ebf83faf054952012
baronvonpurr <robinhood.svensson@hotmail.com>
2024-03-01 17:55:35 +0100

    Add project files.

---
+++ Metoder6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Metod6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en mening");
+            Console.WriteLine(convert(Console.ReadLine()));
+        }
+
+        static string convert(string sentence)
+        {
+            string[] words = sentence.Split(" ");
+            sentence = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    words[i] = words[i].ToUpper() + " ";
+                }
+                else
+                {
+                    words[i] = words[i].ToLower() + " ";
+                }
+                sentence = sentence + words[i];
+            }
+            return sentence;
+        }
+    }
+}
